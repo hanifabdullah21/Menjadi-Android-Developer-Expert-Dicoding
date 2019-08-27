bbe3be23b21febf22fc939cc23e771a841fd3379
singpaulee <hanifabdullah7@gmail.com>
2019-08-27 12:41:24 +0700

    First Commit

---
+++ app/src/main/java/com/singpaulee/made_dicoding/MainActivity.kt
@@ -0,0 +1,14 @@
+import com.singpaulee.made_dicoding.R
+
+em package com.singpaulee.made_dicoding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
