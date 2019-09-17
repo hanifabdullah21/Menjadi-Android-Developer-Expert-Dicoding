d7383e231849019478794606f671b02d95b7c447
hanif <hanifabdullah7@gmail.com>
2019-09-17 19:45:13 +0700

    Add Menu Setting Language

--- app/src/main/java/com/singpaulee/made_dicoding/HomeActivity.kt
+++ app/src/main/java/com/singpaulee/made_dicoding/HomeActivity.kt
@@ -1,12 +1,15 @@
 package com.singpaulee.made_dicoding
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.provider.Settings.ACTION_LOCALE_SETTINGS
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import com.singpaulee.made_dicoding.views.dashboard.DashboardActivity
 import kotlinx.android.synthetic.main.activity_home.*
 import org.jetbrains.anko.intentFor
 import org.jetbrains.anko.toast
+
 
 class HomeActivity : AppCompatActivity(), View.OnClickListener {
 
@@ -21,10 +24,13 @@ class HomeActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     override fun onClick(v: View?) {
-        when(v){
+        when (v) {
             home_cv_list -> startActivity(intentFor<DashboardActivity>())
             home_cv_favorite -> toast(resources.getString(R.string.string_under_contsruction))
-            home_cv_setting -> toast(resources.getString(R.string.string_under_contsruction))
+            home_cv_setting -> {
+                val mIntent = Intent(ACTION_LOCALE_SETTINGS)
+                startActivity(mIntent)
+            }
         }
     }
 }
